58cb944ec5c2f0da3d724135f09fa7302d7abc81
mmahmudul7 <hasanitgroup@gmail.com>
2025-05-07 20:41:32 +0600

    Cart API Desing & Updating Models

--- order/models.py
+++ order/models.py
@@ -6,7 +6,7 @@ from uuid import uuid4
 
 
 class Cart(models.Model):
-    # id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="cart")
     created_at = models.DateTimeField(auto_now_add=True)
 
@@ -19,8 +19,8 @@ class CartItem(models.Model):
     job = models.ForeignKey(Job, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(validators=[MinValueValidator(1)])
 
-    # class Meta:
-    #     unique_together = [['cart', 'job']]
+    class Meta:
+        unique_together = [['cart', 'job']]
 
     def __str__(self):
         return f"{self.quantity} x {self.job.name}"
