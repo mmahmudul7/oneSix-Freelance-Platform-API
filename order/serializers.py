fb7acc300456b13aeb36047d83705f09b364110e
mmahmudul7 <hasanitgroup@gmail.com>
2025-05-12 21:48:13 +0600

    Adding Cart Item

--- order/serializers.py
+++ order/serializers.py
@@ -9,6 +9,33 @@ class SimpleJobSerializer(serializers.ModelSerializer):
         fields = ['id', 'name', 'price']
 
 
+class AddCartItemSerializer(serializers.ModelSerializer):
+    job_id = serializers.IntegerField()
+
+    class Meta:
+        model = CartItem
+        fields = ['id', 'job_id', 'quantity']
+
+    def save(self, **kwargs):
+        cart_id = self.context['cart_id']
+        job_id = self.validated_data['job_id']
+        quantity = self.validated_data['quantity']
+
+        try:
+            cart_item = CartItem.objects.get(cart_id=cart_id, job_id=job_id)
+            cart_item.quantity += quantity
+            self.instance = cart_item.save()
+        except CartItem.DoesNotExist:
+            self.instance = CartItem.objects.create(cart_id=cart_id, **self.validated_data)
+
+        return self.instance
+    
+    def validate_job_id(self, value):
+        if not Job.objects.filter(pk=value).exists():
+            raise serializers.ValidationError(f"Job with id {value} does not exists")
+        return value
+
+
 class CartItemSerializer(serializers.ModelSerializer):
     job = SimpleJobSerializer()
     total_price = serializers.SerializerMethodField(method_name='get_total_price')
