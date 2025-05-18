62372ebf3acfb78bad1b6b75315db10a0df326fb
mmahmudul7 <hasanitgroup@gmail.com>
2025-05-18 07:05:26 +0600

    Registering User API

--- onesix/settings.py
+++ onesix/settings.py
@@ -142,3 +142,9 @@ REST_FRAMEWORK = {
 SIMPLE_JWT = {
    'AUTH_HEADER_TYPES': ('JWT',),
 }
+
+DJOSER = {
+    'SERIALIZERS': {
+        'user_create': 'users.serializers.UserCreateSerializer'
+    }
+}
